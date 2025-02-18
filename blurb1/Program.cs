0017fd4b2ab6b7d6dc5138bde6ae9ed9baf0f160
MEGA <MEGA@gmail.com>
2025-02-18 14:33:58 +0200

    Add project files.

---
+++ blurb1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blurb1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] student =
+            {
+                new Student("Ivan", 14),
+                new Student("Spencer", 19),
+                new Student("Petka", 25),
+                new Student("Silvena", 16)
+            };
+            foreach (var students in student)
+            {
+                Console.WriteLine(students);
+            }
+            Sort(student);
+            Console.WriteLine();
+
+            foreach (var students in student)
+            {
+                Console.WriteLine(students);
+            }
+            Console.ReadKey();
+        }
+        static void Sort( Student[]student)
+        {
+            int n = student.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (student[j].Age > student[j + 1].Age)
+                    {
+                        (student[j], student[j + 1]) = (student[j + 1], student[j]);
+                    }
+                }
+            }
+        }
+        
+        
+    }
+}
---
+++ blurb1/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blurb1
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Student(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+
+        public override string ToString()
+        {
+            return $"name {Name} and the age{Age}";
+        }
+    }
+}
